25889fe28cfa3f4bff58059d67f0d174c652833f
Emil LASLAU <emil.laslau@indsoft.ro>
2023-08-07 21:09:49 +0300

    add types in middleware

--- src/teams/middleware.ts
+++ src/teams/middleware.ts
@@ -1,22 +1,27 @@
-export function loadTeamsRequest() {
+import { Team } from "./models";
+
+export function loadTeamsRequest(): Promise<Team[]> {
   return fetch("http://localhost:3000/teams-json", {
     method: "GET",
     headers: {
-      "Content-Type": "application/json"
-    }
-  }).then(r => r.json());
+      "Content-Type": "application/json",
+    },
+  }).then((r) => r.json());
 }
 
-export function deleteTeamRequest(id: string, callback?: (status: any) => void) {
+export function deleteTeamRequest(
+  id: string,
+  callback?: (status: any) => void
+): Promise<{ success: boolean }> {
   return fetch("http://localhost:3000/teams-json/delete", {
     method: "DELETE",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({ id })
+    body: JSON.stringify({ id }),
   })
-    .then(r => r.json())
-    .then(status => {
+    .then((r) => r.json())
+    .then((status) => {
       if (typeof callback === "function") {
         callback(status);
       }
@@ -24,22 +29,24 @@ export function deleteTeamRequest(id: string, callback?: (status: any) => void) 
     });
 }
 
-export function updateTeamRequest(team) {
+export function updateTeamRequest(team: Team): Promise<{ success: boolean }> {
   return fetch("http://localhost:3000/teams-json/update", {
     method: "PUT",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(team)
-  }).then(r => r.json());
+    body: JSON.stringify(team),
+  }).then((r) => r.json());
 }
 
-export function createTeamRequest(team) {
+export function createTeamRequest(
+  team: Team
+): Promise<{ success: boolean; id: string }> {
   return fetch("http://localhost:3000/teams-json/create", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(team)
-  }).then(r => r.json());
+    body: JSON.stringify(team),
+  }).then((r) => r.json());
 }
